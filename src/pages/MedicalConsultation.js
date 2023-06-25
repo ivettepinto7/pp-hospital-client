29be73ec48933bb9755c355d3ed2d997c9ed1c08
FelixPalacios117 <00055419@uca.edu.sv>
2023-06-25 14:48:11 -0600

    :art: formik form agregado

--- src/pages/MedicalConsultation.js
+++ src/pages/MedicalConsultation.js
@@ -101,67 +101,245 @@ export default function MedicalConsultation() {
       </div>
     );
   };
-
   return (
-    <div className="flex flex-col  items-center justify-center h-screen ">
-      {/*
-       *User creation emergent window
-       */}
-
+    <div className="h-screen max-h-screen bg-black w-full mt-4">
       <UserRecordTable loading={loading2} userRecordsList={userRecordsList} />
-      <CreatePrescription code={userCode} />
-      <h1 className="text-3xl">Consulta</h1>
-      <h2 className="lg:text-xl">
-        <b>Nombre: </b>
-        {fullname}
-      </h2>
-      <h2 className="lg:text-xl">
-        <b>Edad: </b>
-        {age}
-      </h2>
-      <h2 className="lg:text-xl">
-        <b>Género: </b>
-        {gender === "F" ? "Femenino" : "Masculino"}
-      </h2>
-      <h2 className="lg:text-xl">
-        <b>Discutido en cita:</b>
-      </h2>
-      <InputTextarea
-        onChange={(e) => setValue(e.target.value)}
-        rows={5}
-        cols={50}
-      />
-      <br />
-      <div className="lg:flex lg:flex-row lg:justify-evenly xsm:flex-col xsm:justify-center lg:w-1/2">
-        <div className="xsm:m-1 lg:justify-evenly lg:flex">
-          <Button
-            label="Abrir Expediente"
-            className="p-button-info"
-            onClick={() => {
-              getUserRecords(userCode);
-              menuContext.settingEmergentShowRecordState();
-            }}
-          />
+      <Dialog
+        visible={showMessage}
+        onHide={() => setShowMessage(false)}
+        position="top"
+        footer={dialogFooter}
+        showHeader={false}
+        breakpoints={{ "960px": "80vw" }}
+        style={{ width: "30vw" }}
+      >
+        <div className="flex justify-content-center flex-column pt-6 px-3">
+          <i
+            className="pi pi-check-circle"
+            style={{ fontSize: "5rem", color: "var(--green-500)" }}
+          ></i>
+          <p className="ml-4">
+            Prescripción médica agregada con éxito para <b>{fullName}</b>.
+          </p>
         </div>
-        <div className="xsm:m-1 lg:justify-evenly lg:flex">
-          <Button
-            label="Agregar prescripción"
-            className="p-button-secondary"
-            onClick={() => {
-              menuContext.settingEmergentPrescriptionState();
-            }}
-          />
-        </div>
-        <div className="xsm:m-1 lg:justify-evenly lg:flex">
-          <Button
-            label="Finalizar cita"
-            className="p-button-warning"
-            onClick={() => {
-              navigate("/landing/citas-dia");
-            }}
-          />
-        </div>
+      </Dialog>
+      <h1 className="lg:text-4xl md:text-3xl sm:text-2xl xsm:text-xl text-center">
+        Consulta
+      </h1>
+      <div className="text-center grid grid-cols-3 mt-4">
+        <h2 className="lg:text-2xl md:text-xl sm:text-xs xsm:text-xs flex flex-col">
+          <b>Nombre: </b>
+          {fullName}
+        </h2>
+        <h2 className="lg:text-2xl md:text-xl sm:text-xs xsm:text-xs flex flex-col">
+          <b>Edad: </b>
+          {age} años
+        </h2>
+        <h2 className="lg:text-2xl md:text-xl sm:text-xs xsm:text-xs flex flex-col">
+          <b>Género: </b>
+          {gender === "M" ? "Masculino" : "Femenino"}
+        </h2>
       </div>
+
+      <Formik
+        initialValues={{
+          list: [
+            {
+              medicine: -1,
+              doses: "",
+              quantity: "",
+            },
+          ],
+        }}
+        validationSchema={DrugsSchema}
+        onSubmit={(values) => {
+          try {
+            let data = {
+              id_appointment: appointmentId,
+              medicines: values.list,
+              indication: description,
+            };
+            axios
+              .post(
+                process.env.REACT_APP_API_URL +
+                  `doctor/citas-dia/consulta/receta/crear`,
+                data,
+                { headers: { Authorization: `Bearer ${token}` } }
+              )
+              .then((res) => {
+                if (res.status === 201) {
+                  axios
+                    .put(
+                      process.env.REACT_APP_API_URL +
+                        `doctor/citas-dia/consulta/finalizar/${parseInt(
+                          appointmentId
+                        )}`,
+                      null,
+                      { headers: { Authorization: `Bearer ${token}` } }
+                    )
+                    .then((res) => {
+                      if (res.status === 200) {
+                        setShowMessage(true);
+                      }
+                    })
+                    .catch((err) => console.error(err));
+                }
+              })
+              .catch((err) => console.error(err));
+          } catch (error) {
+            throw console.error(error);
+          }
+        }}
+        children={({ values, errors, touched }) => (
+          <Form>
+            <FieldArray
+              name="list"
+              render={(arrayHelpers) => (
+                <>
+                  <div className="lg:grid lg:grid-cols-2 md:grid md:grid-cols-2 m-6">
+                    <div className="mx-2 text-center border-2 rounded-lg py-4 my-2 max-h-100">
+                      <div className="flex justify-center items-center">
+                        <FontAwesomeIcon
+                          className="text-blue-800 text-4xl mx-4"
+                          icon={faCapsules}
+                        />
+                        <h2 className="lg:text-2xl md:text-xl sm:text-xs xsm:text-xs">
+                          <b>Agregar medicamento</b>
+                        </h2>
+                      </div>
+                      <div>
+                        {values.list && values.list.length > 0 ? (
+                          values.list.map((item, index) => (
+                            <div
+                              className="flex justify-center items-center"
+                              key={index}
+                            >
+                              <Field
+                                placeholder="Seleccionar medicina"
+                                className="bg-gray-700 text-white text-sm w-1/4 mx-2"
+                                as="select"
+                                name={`list.${index}.medicine`}
+                                key={`list.${index}.medicine`}
+                              >
+                                <option value={-1} disabled={true}>
+                                  Seleccionar medicamento
+                                </option>
+                                {drugsList &&
+                                  drugsList.map((drug, index) => (
+                                    <option
+                                      key={parseInt(drug.id_drug)}
+                                      value={parseInt(drug.id_drug)}
+                                      defaultValue={undefined}
+                                    >
+                                      {drug.name.toString()}
+                                    </option>
+                                  ))}
+                              </Field>
+                              <ErrorMessage
+                                className="text-xs bg-red-600"
+                                name={`list.${index}.medicine`}
+                              />
+                              <Field
+                                as={CustomInput}
+                                placeholder="Dosis"
+                                min={1}
+                                className="bg-gray-700 text-white text-sm w-1/5 mx-2"
+                                name={`list.${index}.doses`}
+                                key={`list.${index}.doses`}
+                              />
+                              <ErrorMessage
+                                className="text-xs bg-red-600"
+                                name={`list.${index}.doses`}
+                              />
+                              <Field
+                                as={CustomInput}
+                                placeholder="Cantidad"
+                                min={1}
+                                className="bg-gray-700 text-white text-sm w-1/5 mx-2"
+                                name={`list.${index}.quantity`}
+                                key={`list.${index}.quantity`}
+                              />
+                              <ErrorMessage name={`list.${index}.quantity`} />
+                              <button
+                                className="mx-2"
+                                type="button"
+                                onClick={() => {
+                                  if (values.list.length < 9) {
+                                    arrayHelpers.insert(index, "");
+                                  }
+                                }}
+                              >
+                                <FontAwesomeIcon
+                                  className="text-blue-800 text-xl"
+                                  icon={faPlusCircle}
+                                />
+                              </button>
+                              <button
+                                className="mx-2"
+                                type="button"
+                                onClick={() => arrayHelpers.remove(index)}
+                              >
+                                <FontAwesomeIcon
+                                  className="text-red-600 text-xl"
+                                  icon={faTrashAlt}
+                                />
+                              </button>
+                            </div>
+                          ))
+                        ) : (
+                          <button
+                            className="text-sm underline"
+                            type="button"
+                            onClick={() => arrayHelpers.push("")}
+                          >
+                            Agregar
+                          </button>
+                        )}
+                      </div>
+                    </div>
+                    <div className="mx-2 text-center border-2 rounded-lg py-4 my-2">
+                      <div className="flex justify-center items-center">
+                        <FontAwesomeIcon
+                          className="text-yellow-500 text-4xl mx-4"
+                          icon={faPenToSquare}
+                        />
+                        <h2 className="lg:text-2xl md:text-xl sm:text-xs xsm:text-xs">
+                          <b>Nota adicional de la consulta</b>
+                        </h2>
+                      </div>
+                      <InputTextarea
+                        placeholder="Ej: Regresar a consulta al terminar tratamiento."
+                        className="mt-4"
+                        onChange={(e) => setDescription(e.target.value)}
+                        rows={5}
+                        cols={50}
+                      />
+                    </div>
+                  </div>
+                  <div className="lg:flex md:flex lg:justify-around md:justify-around sm:justify-center xsm:justify-center m-6">
+                    <button
+                      type="button"
+                      className="flex justify-center items-center text-white text-xl p-2 rounded-lg tracking-wide font-bold focus:outline-none focus:shadow-outline hover:bg-yellow-600 shadow-lg bg-yellow-700 cursor-pointer transition ease-in duration-300"
+                      onClick={() => {
+                        getUserRecords(userCode.id_person);
+                        menuContext.settingEmergentShowRecordState();
+                      }}
+                    >
+                      Abrir expediente
+                    </button>
+                    <button
+                      className="flex justify-center items-center text-white text-xl p-2 rounded-lg tracking-wide font-bold focus:outline-none focus:shadow-outline hover:bg-indigo-600 shadow-lg bg-blue-800 cursor-pointer transition ease-in duration-300"
+                      type="submit"
+                    >
+                      Finalizar cita
+                    </button>
+                  </div>
+                </>
+              )}
+            />
+          </Form>
+        )}
+      />
     </div>
   );
 }
