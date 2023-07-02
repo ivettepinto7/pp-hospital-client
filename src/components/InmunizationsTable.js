ceaf08f2da3929ea0f3e5fa9f62582b4ebc1af8d
Dan920-Dev <00138218@uca.edu.sv>
2023-07-01 21:38:44 -0600

    add Table function

--- src/components/InmunizationsTable.js
+++ src/components/InmunizationsTable.js
@@ -6,3 +6,25 @@ import { UserContext } from "../contexts/UserContext/UserContext";
 import { DataTable } from "primereact/datatable";
 import { Column } from 'primereact/column';
 import './cssFiles/DataTable.css';
+
+
+export default function InmunizationsTable() {
+  const { token } = useContext(UserContext);
+
+  const dt = useRef(null);
+  const [loading, setLoading] = useState(true);
+  const [inmunizationsList, setInmunizationsList] = useState([]);
+
+  useEffect(() => {
+    try {
+      axios.get(process.env.REACT_APP_API_URL + "patient/expediente/inmunizaciones", { headers: { Authorization: `Bearer ${token}` } })
+        .then(res => {
+          if (res.status === 200) {
+            setInmunizationsList(res.data);
+            setLoading(false);
+          }
+        }).catch(err => console.error(err));
+    } catch (error) {
+      throw console.error(error);
+    }
+  })
