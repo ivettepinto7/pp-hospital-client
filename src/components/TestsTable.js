99910e08ae890cd2f7f5ddb38536edac6aecedc4
Dan920-Dev <00138218@uca.edu.sv>
2023-07-01 21:46:31 -0600

    adding testTable function

--- src/components/TestsTable.js
+++ src/components/TestsTable.js
@@ -6,3 +6,26 @@ import { UserContext } from "../contexts/UserContext/UserContext";
 import { DataTable } from "primereact/datatable";
 import { Column } from 'primereact/column';
 import './cssFiles/DataTable.css';
+
+
+export default function TestsTable() {
+  const { token } = useContext(UserContext);
+
+  const [loading, setLoading] = useState([]);
+  const [doneTestsList, setDoneTestsList] = useState([]);
+  const dt = useRef(null);
+
+  useEffect(() => {
+    try {
+      axios.get(process.env.REACT_APP_API_URL + "patient/expediente/examenes-realizados", { headers: { Authorization: `Bearer ${token}` } })
+        .then(res => {
+          if (res.status === 200) {
+            setDoneTestsList(res.data);
+            setLoading(false);
+          }
+        }).catch(err => console.error(err));
+    } catch (error) {
+      throw console.error(error);
+    }
+  })
+  
