3be9f929eaa65ef554bf22d1786131db9ffb91b0
Dan920-Dev <00138218@uca.edu.sv>
2023-07-01 21:44:17 -0600

    adding test function

--- src/pages/Tests.js
+++ src/pages/Tests.js
@@ -1,3 +1,11 @@
 import React from "react";
 
 import TestTable from "../components/TestsTable";
+
+export default function Tests() {
+  return (
+    <div className="datatable-crud-demo">
+      <TestTable />
+    </div>
+  );
+}
