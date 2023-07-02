9cd081c9165667d3e584f601f53e1bf893214cd0
Dan920-Dev <00138218@uca.edu.sv>
2023-07-01 21:45:49 -0600

    adding imports

---
+++ src/components/TestsTable.js
@@ -0,0 +1,8 @@
+import React, { useState, useContext, useEffect, useRef } from "react";
+import axios from "axios";
+import { UserContext } from "../contexts/UserContext/UserContext";
+
+//Components imports
+import { DataTable } from "primereact/datatable";
+import { Column } from 'primereact/column';
+import './cssFiles/DataTable.css';
