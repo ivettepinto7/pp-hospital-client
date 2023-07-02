c832dd45bce80e0252cdffde751b5b9e940d408a
Dan920-Dev <00138218@uca.edu.sv>
2023-07-01 21:39:30 -0600

    adding functions getAge and ageBodyTemplate

--- src/components/InmunizationsTable.js
+++ src/components/InmunizationsTable.js
@@ -28,3 +28,43 @@ export default function InmunizationsTable() {
       throw console.error(error);
     }
   })
+
+  const header = (
+    <div className="table-header">
+      <h5 className="mx-0 my-1">Vacunas aplicados</h5>
+    </div>
+  );
+
+  function getAge(dateString,timestamp) 
+{
+    var t = new Date(timestamp);
+    var birthDate = new Date(dateString);
+    var age = t.getFullYear() - birthDate.getFullYear();
+    var m = t.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && t.getDate() < birthDate.getDate())) 
+    {
+        age--;
+    }
+    return age;
+}
+  
+  const ageBodyTemplate = (rowData) => {
+    return getAge(rowData.id_patient.birthdate, rowData.timestamp);
+  }
+
+  return (
+    <div className="w-full overflow-hidden">
+      <div className="card">
+        <DataTable showGridlines ref={dt} value={inmunizationsList}
+          dataKey="id_inmunization" paginator rows={10} rowsPerPageOptions={[5, 10, 25]} totalRecords={inmunizationsList.length}
+          paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
+          currentPageReportTemplate="Mostrando {first} - {last} de {totalRecords} inmunizaciones"
+          loading={loading} header={header} responsiveLayout="scroll">
+          <Column field="id_vaccine.name" header="Nombre de vacuna" style={{ minWidth: '12rem' }}></Column>
+          <Column field="id_patient.birthdate" header="Edad de aplicación" body={ageBodyTemplate} style={{ minWidth: '12rem' }}></Column>
+          <Column field="timestamp" header="fecha" style={{ minWidth: '10rem' }}></Column>
+        </DataTable>
+      </div>
+    </div>
+  )
+}
